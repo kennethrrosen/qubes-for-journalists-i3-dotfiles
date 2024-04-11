5ee1ca8a774c2c3eac5c08652f63f001af058609
ooops <kennethrrosen@proton.me>
2024-04-11 18:56:39 +0000

    Delete .local directory

--- .local/bin/login_screencapture.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/sh
-# Description: Take screenshot for the login screen
-sleep 10s; DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 xwd -root -out ~/loginscreen.xwd; convert ~/loginscreen.xwd ~/loginscreen.png; rm ~/loginscreen.xwd
